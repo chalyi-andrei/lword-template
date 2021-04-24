8ba3fc044f813c82cbe2da3915ee24a55e52483d
max_myint <maxym.lozovoy@myntelligence.com>
2021-04-24 21:49:39 +0300

    fix: color paddings margin

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,28 +1,33 @@
-import { Link } from 'react-router-dom';
-import './Tabs.scss';
+import { Link } from "react-router-dom";
+import "./Tabs.scss";
 
 const Tabs = () => (
-	<div class="tabs-container">
-		<div class="tabs-block">
-			<div class="tabs-navigation">
-				<Link title="Последние" class="tabs-navigation__item" to="/">
-					Последние
-				</Link>
-				<Link title="По книге" class="tabs-navigation__item tab-item--active" to="books">
-					По книге
-				</Link>
-				<Link title="По темам" class="tabs-navigation__item" to="themes">
-					По темам
-				</Link>
-				<Link title="Серии" class="tabs-navigation__item" to="/series">
-					Серии
-				</Link>
-				<Link title="По автору" class="tabs-navigation__item" to="/author">
-					По автору
-				</Link>
-			</div>
-		</div>
-	</div>
+  <div class="tabs-container">
+    <div class="tabs-block">
+      <div class="tabs-title">Выбрать Проповеди</div>
+      <div class="tabs-navigation">
+        <Link title="Последние" class="tabs-navigation__item" to="/">
+          Последние
+        </Link>
+        <Link
+          title="По книге"
+          class="tabs-navigation__item tab-item--active"
+          to="books"
+        >
+          По книге
+        </Link>
+        <Link title="По темам" class="tabs-navigation__item" to="themes">
+          По темам
+        </Link>
+        <Link title="Серии" class="tabs-navigation__item" to="/series">
+          Серии
+        </Link>
+        <Link title="По автору" class="tabs-navigation__item" to="/author">
+          По автору
+        </Link>
+      </div>
+    </div>
+  </div>
 );
 
 export default Tabs;
--- src/pages/Sermons/Sermons.jsx
+++ src/pages/Sermons/Sermons.jsx
@@ -1,71 +1,87 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-import Paginations from '../../components/Pagination/Pagingtion';
-import Tabs from '../../components/Tabs/Tabs';
-import './Sermons.scss';
-import bgImg from '../../img/background-sermon.png';
+import Paginations from "../../components/Pagination/Pagingtion";
+import Tabs from "../../components/Tabs/Tabs";
+import "./Sermons.scss";
+import bgImg from "../../img/background-sermon.png";
 
 const SermoonPage = () => {
-	return (
-		<div class="container sermons-page">
-			<div class="wrapper">
-				<div class="sermons-block">
-					<div class="sermons-title">Выбрать Проповеди</div>
+  return (
+    <div class="container sermons-page">
+      <div class="wrapper">
+        <div class="sermons-block">
+          <Tabs />
+          <div class="sermons-list">
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+            <Link class="sermons-list-item" to="/sermoons/1">
+              <img class="sermons-list-item__image" src={bgImg} alt="sermon" />
+              <div class="sermons-list-item__title">
+                Перемены - дело всей общины
+              </div>
+              <div class="sermons-list-item__date">14 июля 2020 г.</div>
+            </Link>
+          </div>
+        </div>
 
-					<Tabs />
-					<div class="sermons-list">
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-						<Link class="sermons-list-item" to="/sermoons/1">
-							<img class="sermons-list-item__image" src={bgImg} alt="sermon" />
-							<div class="sermons-list-item__title">Перемены - дело всей общины</div>
-							<div class="sermons-list-item__date">14 июля 2020 г.</div>
-						</Link>
-					</div>
-				</div>
-
-				<Paginations />
-			</div>
-		</div>
-	);
+        <Paginations />
+      </div>
+    </div>
+  );
 };
 
 export default SermoonPage;
